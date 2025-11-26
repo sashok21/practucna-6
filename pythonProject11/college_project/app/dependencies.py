c955c635dbea90874d98b0096c30978b62d8cc6a
Sasha Shkvarla <118696221+sashok21@users.noreply.github.com>
2025-11-26 11:49:18 +0200

    Delete pythonProject11/college_project directory

--- pythonProject11/college_project/app/dependencies.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.core.settings.db import db
-
-SessionDepend = Annotated[AsyncSession, Depends(db.get_session)]
